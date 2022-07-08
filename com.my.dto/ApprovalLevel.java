057b952b043e613806f187a89c594506d9cd1922
Jiyeon <jiyeon.backend@gmail.com>
2022-07-08 19:04:36 +0900

    [추가] DTO

---
+++ com.my.dto/ApprovalLevel.java
@@ -0,0 +1,18 @@
+package com.my.dto;
+
+public class ApprovalLevel {
+	private Employee employee;
+
+	public ApprovalLevel() {
+		
+	} 
+	
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+		
+}
---
+++ com.my.dto/Department.java
@@ -0,0 +1,29 @@
+package com.my.dto;
+
+public class Department {
+	private int departmentId;
+	private String departmentName;
+	
+	public Department() {
+		
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	
+	
+	
+}
