9a1ee730792a85f40da6f4042897cc7b538d853a
Bella <96408941+KMJbella@users.noreply.github.com>
2022-07-10 16:36:06 +0900

    [Modified] NoticeOracleRepository.java

--- back/src/main/java/com/my/repository/NoticeOracleRepository.java
+++ back/src/main/java/com/my/repository/NoticeOracleRepository.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.my.dto.Employee;
 import com.my.dto.Notice;
 import com.my.exception.SelectException;
 import com.my.sql.MyConnection;
@@ -15,13 +16,15 @@ import com.my.sql.MyConnection;
 public class NoticeOracleRepository implements NoticeRepository {
 	
 	@Override
-	public List<Notice> selectNoticeId(int startRow, int endRow) throws SelectException {
+	public List<Notice> selectNoticesByWritingtime(int startRow, int endRow) throws SelectException {
 		List<Notice> notices = new ArrayList<Notice>();
 		Connection con = null;
 		PreparedStatement pstmt = null;
 	    ResultSet rs = null;
 
-	    String selectSQL = "select notice_title, notice_id from notice where rownum between ? AND ?";
+	    String selectSQL = "select * from notice n\r\n"
+	    		+ "join employee e on (e.employee_id = n.employee_id)\r\n"
+	    		+ "where rownum between ? AND ? ";
 		
 	    try { 
 				con = MyConnection.getConnection();	
@@ -30,20 +33,21 @@ public class NoticeOracleRepository implements NoticeRepository {
 			    pstmt.setInt(2, endRow);
 			    rs = pstmt.executeQuery();
 			    
-				if (rs.next()) {
-					int noticeId = rs.getInt("notice_id");
-					int employeeId = rs.getInt("employee_id");
-					String noticeCnt = rs.getString("notice_cnt");
-					String noticeTitle = rs.getString("notice_title");
-					java.sql.Date noticeTime = rs.getDate("notice_time");
+				while(rs.next()== true)  {
+					Notice notice = new Notice();
+					Employee employee = new Employee();
+			        notice.setNoticeId(rs.getInt("notice_id"));
+					employee.setEmployeeId(rs.getInt("employee_id"));
+					notice.setEmployee(employee);
+					notice.setNoticeCnt(rs.getString("notice_cnt"));
+					notice.setNoticeTitle(rs.getString("notice_title"));
+					notice.setNoticeTime(rs.getDate("notice_time"));
 					
-//					Notice n = new Notice(noticeId, employeeId, noticeCnt, noticeTitle, noticeTime);
-				  //List? 
+					notices.add(notice); // list에 방금 생성한 객체 넣기
 					
-					return notices;
-				}else {
-					throw new SelectException();
 				}
+					
+				
 			     
 			 } catch (SQLException e) {
 				 e.printStackTrace();
@@ -51,6 +55,7 @@ public class NoticeOracleRepository implements NoticeRepository {
 			 } finally {
 			      MyConnection.close(rs, pstmt, con);
 			    }    
+	    return notices;
 	}
 	
 
@@ -77,29 +82,28 @@ public class NoticeOracleRepository implements NoticeRepository {
 			    pstmt.setString(3, noticeTitle);
 			    pstmt.executeQuery();
 			    
-			    if (rs.next()) {
-					int noticeId = rs.getInt("notice_id");
-					int employeeId = rs.getInt("employee_id");
-					String noticeCnt = rs.getString("notice_cnt");
-//					String noticeTitle = rs.getString("notice_title");
-					Date noticeTime = rs.getDate("notice_time");
+			    while(rs.next())  {
+					Notice notice = new Notice();
+					Employee employee = new Employee();
+			        notice.setNoticeId(rs.getInt("notice_id"));
+					employee.setEmployeeId(rs.getInt("employee_id"));
+					notice.setEmployee(employee);
+					notice.setNoticeCnt(rs.getString("notice_cnt"));
+					notice.setNoticeTitle(rs.getString("notice_title"));
+					notice.setNoticeTime(rs.getDate("notice_time"));
 					
-//					Notice n = new Notice(noticeId, employeeId, noticeCnt, noticeTitle, noticeTime);
-				  //List? 
+					notices.add(notice); // list에 방금 생성한 객체 넣기
 					
-					return notices;
-				}else {
-					throw new SelectException("검색어를 포함한 제목의 공지사항이 없습니다");
 				}
-			     
-			 } catch (SQLException e) {
+		 
+			} catch (SQLException e) {
 				 e.printStackTrace();
 				 throw new SelectException(e.getMessage());
 			 } finally {
 			      MyConnection.close(rs, pstmt, con);
 			    }    
+	    return notices;
 	}
-
 	
 	@Override
 	public Notice selectByNoticeId(int noticeId) throws SelectException {
@@ -147,10 +151,3 @@ public class NoticeOracleRepository implements NoticeRepository {
 			    }    
 	}
 }
-	
-	
-	
-	
-	
-	
-	
