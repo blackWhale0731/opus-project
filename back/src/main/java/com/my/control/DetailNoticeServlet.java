0b66ba9f8f75b846b8f46f33d0c410ce4b85664f
KMJbella <kmj960803@naver.com>
2022-07-13 18:53:13 +0900

    [Modify] notice

---
+++ back/src/main/java/com/my/control/DetailNoticeServlet.java
@@ -0,0 +1,70 @@
+package com.my.control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my.dto.Notice;
+import com.my.exception.SelectException;
+import com.my.repository.NoticeOracleRepository;
+import com.my.repository.NoticeRepository;
+
+/**
+ * Servlet implementation class DetailNoticeServlet
+ */
+@WebServlet("/detailnotice")
+public class DetailNoticeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * 공지사항 상세보기
+	 */
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String result = "";
+		
+		//1. 요청전달데이터 얻기
+//				int notice_id = request.getParameter("notice_id");
+				
+				
+				//2. DB
+				NoticeRepository repository = new NoticeOracleRepository();
+				try {
+				Notice n = repository.selectByNoticeId(1);
+				Map<String, Object> map = new HashMap<>();
+				map.put("status", 1);
+				map.put("n", n);
+				
+				ObjectMapper mapper = new ObjectMapper();
+				
+				result = mapper.writeValueAsString(map);
+				System.out.println("result : " + result);
+				out.print(result);
+				
+				} catch (SelectException e) {
+					e.printStackTrace();
+					Map<String, Object> map = new HashMap<>();
+					map.put("status", 0);
+					map.put("msg", e.getMessage());
+					
+					ObjectMapper mapper = new ObjectMapper();
+					result = mapper.writeValueAsString(map);
+					System.out.println("result : " + result);
+					out.print(result);
+				}	
+				
+			}
+		
+	}
+
---
+++ back/src/main/java/com/my/control/ViewNoticeServlet.java
@@ -0,0 +1,83 @@
+package com.my.control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my.dto.Employee;
+import com.my.dto.Notice;
+import com.my.exception.SelectException;
+import com.my.repository.NoticeOracleRepository;
+import com.my.repository.NoticeRepository;
+
+/**
+ * Servlet implementation class NoticeServlet
+ * 공지사항 10개씩 출력
+ */
+
+@WebServlet("/viewnotice")
+public class ViewNoticeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		//1. 요청 전달데이터 얻기
+//		String start_row = request.getParameter("start_row");
+//		String end_row = request.getParameter("end_row");
+		
+		//2. DB
+		NoticeRepository repository = new NoticeOracleRepository();
+		List<Notice> notices;
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> map = new HashMap<>();
+		try {
+			notices = repository.selectNoticesByWritingtime(1, 10);
+			map.put("status", 1);
+			map.put("notices", notices);
+		} catch (SelectException e) {
+			e.printStackTrace();
+//				notices = new ArrayList<>();
+			map.put("status", 0);
+			
+		}
+		
+		String result = mapper.writeValueAsString(map);
+		System.out.println(result);
+//		String result = "[";
+//		for(int i=0; i<notices.size(); i++) {
+//			if(i > 0) {
+//			           result +=",";
+//			}
+//			Notice n = notices.get(i);
+//			Employee em = new Employee();
+//			result += "{";
+//			result += "\"notice_id\":"; result += "\"" + n.getNoticeId()  + "\""; result +=",";
+//			result += "\"employee_id\":"; result += "\"" + em.getEmployeeId()  + "\""; result +=",";	
+////			result += "\"notice_cnt\":"; result += "\"" + n.getNoticeCnt()  + "\""; result +=",";	
+//			result += "\"notice_title \":"; result += "\"" + n.getNoticeTitle()  + "\""; result +=",";	
+//			result += "\"notice_time\":"; result += n.getNoticeTime(); 
+//			result += "}";
+//		}
+//		result += "]";
+		out.print(result);
+	}
+	
+	
+	
+	}
