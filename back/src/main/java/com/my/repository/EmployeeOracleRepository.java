1f9c94c526fba858c413cefa2d07a30f2104bb9e
Jiyeon <jiyeon.backend@gmail.com>
2022-07-14 10:02:02 +0900

    [Modify] employee 인터페이스 리퍼지토리 매서드 제거

--- back/src/main/java/com/my/repository/EmployeeOracleRepository.java
+++ back/src/main/java/com/my/repository/EmployeeOracleRepository.java
@@ -13,71 +13,7 @@ import com.my.sql.MyConnection;
 
 public class EmployeeOracleRepository implements EmployeeRepository {
 
-	@Override
-	public Employee selectByIdAndPwd(int employeeId, String employeePassword) throws SelectException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String selectSQL = "SELECT * FROM employee WHERE employee_id= ? AND employee_password= ?";
-		
-		try {
-			con = MyConnection.getConnection();
-			pstmt = con.prepareStatement(selectSQL);
-			rs = pstmt.executeQuery();
-			
-			if (rs.next()) {
-				String employeeNameKr = rs.getString("employee_naame_kr");
-				String employeeNameEng = rs.getString("employee_name_eng");
-				java.sql.Date employeeHiredate = rs.getDate("employee_hiredate");
-				int employeePhonenumber = rs.getInt("employee_phonenumber");
-				String employeeEmail = rs.getString("employee_email");
-				String employeeAddress = rs.getString("employee_address");
-				String employeeGender = rs.getString("employee_gender");
-				java.sql.Date employeeBirthday = rs.getDate("employee_birthday"); // util?? MyWeb의ProductOracleRepository 참조
-				int employeeResign = rs.getInt("employee_resign");
-				int employeeAuthority = rs.getInt("employee_authority");
 
-				String departmentName = rs.getString("department_name");
-				int departmentId = rs.getInt("employeeId");
-
-				Department d = new Department(departmentId, departmentName);
-				Employee em = new Employee(employeeId, employeePassword, employeeNameKr, employeeNameEng,
-						employeeHiredate, employeePhonenumber, employeeEmail, employeeAddress, employeeGender,
-						employeeBirthday, employeeResign, employeeAuthority, d);
-
-				return em;
-			} else {
-				throw new SelectException("사번 혹은 비밀번호가 일치하지 않습니다");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new SelectException(e.getMessage());
-			
-		} finally {
-			MyConnection.close(rs, pstmt, con);
-		}
-
-	}
-
-	@Override
-	public void update(int employeeId, String employeePassword) throws UpdateException {
-		Connection con = null; // private?
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String updateSQL = "UPDATE employee SET employee_password = ? WHERE employee_id = ?";
-		Employee employee = new Employee();
-		try {
-			con = MyConnection.getConnection();
-			pstmt = con.prepareStatement(updateSQL);
-			pstmt.setString(1, employee.getEmployeePassword());
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new UpdateException("비밀번호는 최소  6자리 이상이어야합니다");
-		}finally {
-			MyConnection.close(rs, pstmt, con);
-		}
-	}
 
 	@Override
 	public Employee selectMyPage(int employeeId) throws SelectException {
@@ -85,17 +21,16 @@ public class EmployeeOracleRepository implements EmployeeRepository {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 
-		String selectSQL = "SELECT e.employee_name_kr, e.employee_name_eng, e.employee_birthday, "
-				+ "e.employee_gender, e.employee_address, e.employee_id, e.employee_password, d.department_name, "
-				+ "e.employee_phonenumber, e.employee_hiredate, e.employee_email \r\n"
+		String selectSQL = "SELECT * \r\n"
 				+ "FROM employee e JOIN department d ON e.department_id=d.department_id\r\n"
-				+ "WHERE e.employee_id = ?";
+				+ "WHERE e.employee_id = ? ";
 		try {
 			con = MyConnection.getConnection();
 			pstmt = con.prepareStatement(selectSQL);
+			pstmt.setInt(1, employeeId );
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
-				String employeeNameKr = rs.getString("employee_naame_kr");
+				String employeeNameKr = rs.getString("employee_name_kr");
 				String employeeNameEng = rs.getString("employee_name_eng");
 				java.sql.Date employeeBirthday = rs.getDate("employee_birthday"); // util??
 				String employeeGender = rs.getString("employee_gender");
@@ -105,14 +40,14 @@ public class EmployeeOracleRepository implements EmployeeRepository {
 				java.sql.Date employeeHiredate = rs.getDate("employee_hiredate");
 				String employeeEmail = rs.getString("employee_email");
 
-				int departmentId = rs.getInt("employeeId");
+				int departmentId = rs.getInt("department_id");
 				String departmentName = rs.getString("department_name");
 
-				Department d = new Department(departmentId, departmentName);
-				Employee em = new Employee(employeeId, employeePassword, employeeNameKr, employeeNameEng,
+				Department department = new Department(departmentId, departmentName);
+				Employee employee = new Employee(employeeId, employeePassword, employeeNameKr, employeeNameEng,
 						employeeHiredate, employeePhonenumber, employeeEmail, employeeAddress, employeeGender,
-						employeeBirthday, d);
-				return em;
+						employeeBirthday, department);
+				return employee;
 
 			} else {
 				throw new SelectException();
