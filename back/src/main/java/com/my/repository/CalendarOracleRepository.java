dbbcd43035f25bf3c4ee046c5d4080854d9ee579
Jiyeon <jiyeon.backend@gmail.com>
2022-07-14 09:59:09 +0900

    [Modify] 캘린더 리퍼지토리

--- back/src/main/java/com/my/repository/CalendarOracleRepository.java
+++ back/src/main/java/com/my/repository/CalendarOracleRepository.java
@@ -10,11 +10,11 @@ import java.util.List;
 
 import com.my.dto.CalType;
 import com.my.dto.Calendar;
+import com.my.dto.Department;
 import com.my.dto.Employee;
 import com.my.exception.InsertException;
 import com.my.exception.SelectException;
-
-import sql.MyConnection;
+import com.my.sql.MyConnection;
 
 public class CalendarOracleRepository implements CalendarRepository {
 
@@ -30,15 +30,16 @@ public class CalendarOracleRepository implements CalendarRepository {
 			//int rowcnt = pstmt.executeUpdate(insertSQL);
 			//System.out.println(rowcnt+"건이 추가되었습니다");
 			
-		pstmt = con.prepareStatement(insertSQL);
-		pstmt.setInt(1, calendar.getCalNo());
-		pstmt.setInt(2, calendar.getEmployee().getEmployeeId());
-		pstmt.setInt(3, calendar.getCalType().getCalType());
-		pstmt.setInt(4, calendar.getCalRevealed());
-		pstmt.setString(5, calendar.getCalCnt());
-		pstmt.setDate(6, new java.sql.Date(calendar.getCalStart().getTime())); //Date확인필요
-		pstmt.setDate(7, new java.sql.Date(calendar.getCalEnd().getTime()));
-		
+			pstmt = con.prepareStatement(insertSQL);
+//			pstmt.setInt(1, calendar.getCalNo());
+			pstmt.setInt(1, calendar.getEmployee().getEmployeeId());
+			pstmt.setInt(2, calendar.getCalType().getCalType());
+			pstmt.setInt(3, calendar.getCalRevealed());
+			pstmt.setString(4, calendar.getCalCnt());
+			pstmt.setDate(5, new java.sql.Date(calendar.getCalStart().getTime())); //Date확인필요
+			pstmt.setDate(6, new java.sql.Date(calendar.getCalEnd().getTime()));
+			pstmt.executeUpdate();
+			System.out.println("INSERT CALENDAR OK");
 		} catch (SQLException e) {
 			throw new InsertException(e.getMessage());
 		} catch (Exception e) {
@@ -50,90 +51,179 @@ public class CalendarOracleRepository implements CalendarRepository {
 	}
 
 	@Override
-	public List<Calendar> selectCalByEmployeeId(Date Month, int employeeId, int MonthButton) throws SelectException {
+	public List<Calendar> selectCalMyExcludeDept(String ym, int employeeId) throws SelectException {
 			
 		List<Calendar> calendars = new ArrayList<Calendar>();
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
+			String selectSQL = 
+					"SELECT c.employee_id, "
+					+"c.cal_revealed, "
+					+ "c.cal_type, "
+					+ "t.cal_type_name, "
+					+ "c.cal_cnt, "
+					+ "c.cal_start, "
+					+ "c.cal_end\r\n"
+					+ "FROM calendar c JOIN employee e ON(c.employee_id = e.employee_id)\r\n"
+					+ "                	      JOIN cal_type t ON(c.cal_type = t.cal_type)\r\n"
+					+ "WHERE c.employee_id = ? \r\n"
+					+ "AND(? = TO_CHAR(cal_start, 'MM') \r\n"
+					+ "OR  ? = TO_CHAR(cal_end, 'MM'))";
+			
 			con = MyConnection.getConnection();
-			String selectSQL = "select employee_id, cal_revealed, cal_type, cal_type_name, cal_cnt, cal_start, cal_end\r\n"
-					+ "from calendar\r\n"
-					+ "where employee_id = ?\r\n"
-					+ "AND (TO_CHAR(add_months(?, ?), 'MM') = TO_CHAR(cal_start, 'MM') OR TO_CHAR(add_months(?,0), 'MM') = TO_CHAR(cal_end, 'MM'));\r\n";
 			pstmt = con.prepareStatement(selectSQL);
+			pstmt.setInt(1, employeeId);
+			pstmt.setString(2, ym);
+			pstmt.setString(3, ym);
 			rs = pstmt.executeQuery();
 			
 			while(rs.next()) {
-				
-//				int employeeId = rs.getInt("employee_id");
+				System.out.println("in rs next");
+				Employee em = new Employee(employeeId);
 				int calRevealed = rs.getInt("cal_revealed");
+				
 				int calType = rs.getInt("cal_type");
-				String calCnt = rs.getString("cal_cnt");
-				Date calStart = rs.getDate("cal_start");
-				Date calEnd = rs.getDate("cal_end");
 				String calTypeName = rs.getString("cal_type_name");
 				
 				CalType ct = new CalType(calType, calTypeName);
-				Employee e = new Employee(employeeId);
-				Calendar cal = new Calendar(e, calRevealed, ct, calCnt, calStart, calEnd );
+				String calCnt = rs.getString("cal_cnt");
+				Date calStart = rs.getDate("cal_start");
+				Date calEnd = rs.getDate("cal_end");
+				
+				Calendar cal = new Calendar(em, calRevealed, ct, calCnt, calStart, calEnd);
 				calendars.add(cal);
 				
-				
 			}
-		} catch (SQLException e) {
-			throw new SelectException();
-		} catch (Exception e){
+			return calendars;
+		} catch (SQLException e){
 			e.printStackTrace();
+			throw new SelectException();
 		} finally {
 			MyConnection.close(rs, pstmt, con);
 		}
-		return calendars;
+		
 	}
-
+	
 	@Override
-	public List<Calendar> selectCalByDepartmentId(Date Month, int employeeId, int MonthButton) throws SelectException {
+	public List<Calendar> selectCalBoth(String ym, int employeeId) throws SelectException {
 
 		List<Calendar> calendars = new ArrayList<Calendar>();
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
+			
+			String selectSQL = " SELECT * "
+					+ "FROM calendar c JOIN employee e ON(c.employee_id = e.employee_id)"
+					+ "JOIN cal_type t ON(c.cal_type = t.cal_type)\r\n"
+					+ "WHERE c.cal_revealed=1 AND  e.department_id = (SELECT e.department_id FROM employee e WHERE e.employee_id =  ?)"
+					+ "OR (c.cal_revealed=0 AND c.employee_id = ?) "
+					+ "AND(? = TO_CHAR(cal_start, 'MM') OR (? = TO_CHAR(cal_end, 'MM')))";
+			
 			con = MyConnection.getConnection();
-			String selectSQL = "select c.employee_id, c.cal_revealed, c.cal_type, c.cal_type_name, c.cal_cnt, c.cal_start, c.cal_end \r\n"
-					+ "from calendar c JOIN employee e ON(c.employee_id = e.employee_id)\r\n"
-					+ "WHERE cal_revealed=1 AND  e.department_id = (SELECT department_id FROM employee WHERE employee_id = ?)\r\n"
-					+ "AND(TO_CHAR(add_months(?, 0), 'MM') = TO_CHAR(cal_start, 'MM') OR\r\n"
-					+ "TO_CHAR(add_months(?, 0), 'MM') = TO_CHAR(cal_end, 'MM'));\r\n";
 			pstmt = con.prepareStatement(selectSQL);
+			
+			pstmt.setInt(1, employeeId);
+			pstmt.setInt(2, employeeId);
+			pstmt.setString(3,  ym);
+			pstmt.setString(4,  ym);
 			rs = pstmt.executeQuery();
 			
 			while(rs.next()) {
-				//calendars.add(new Calendar(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getString(4), rs.getDate(5), rs.getDate(6)));
-			
+				 //테스트
+	        	System.out.println("in rs next()");
+	        	 
+				String employeeNameKr = rs.getString("employee_name_kr");
+				
 				int calRevealed = rs.getInt("cal_revealed");
-				int calType = rs.getInt("cal_type");
 				String calCnt = rs.getString("cal_cnt");
 				Date calStart = rs.getDate("cal_start");
 				Date calEnd = rs.getDate("cal_end");
+				
+				int calType = rs.getInt("cal_type");
 				String calTypeName = rs.getString("cal_type_name");
 				
-				CalType ct = new CalType(calType, calTypeName);
-				Employee e = new Employee(employeeId);
-				Calendar cal = new Calendar(e, calRevealed, ct, calCnt, calStart, calEnd );
+				int departmentId = rs.getInt("department_id");
+				Department department = new Department(departmentId);
+				CalType CalType = new CalType(calType, calTypeName);
+				Employee employee = new Employee(employeeId, employeeNameKr, department);
+				Calendar cal = new Calendar(employee, calRevealed, CalType, calCnt, calStart, calEnd  );
 				calendars.add(cal);
 				
 			}
+			return calendars;
 			
 		} catch (SQLException e) {
-			throw new SelectException();
-		} catch (Exception e){
 			e.printStackTrace();
-		} finally {
+			throw new SelectException(e.getMessage());
+		}finally {
 			MyConnection.close(rs, pstmt, con);
 		}
-		return calendars;
+		
 	}
+
+	 //부서일정 반환
+	   @Override
+	   public List<Calendar> selectCalDeptExcludeMy(String ym, int employeeId) throws SelectException {
+	      List<Calendar> calendars = new ArrayList<Calendar>();
+	      Connection con = null;
+	      PreparedStatement pstmt = null;
+	      ResultSet rs = null;
+	      
+	      try {
+	         	         String selectSQL = "SELECT * "
+	               + "FROM calendar c JOIN employee e ON (c.employee_id = e.employee_id)\r\n"
+	               + "                JOIN cal_type t ON (c.cal_type = t.cal_type)\r\n"
+	               + "WHERE c.cal_revealed =1 "
+	               + "              AND e.department_id = (SELECT department_id FROM employee WHERE employee_id = ?) "
+	               + "              AND e.employee_id != ? \r\n"
+	               + "              AND(? = TO_CHAR(cal_start, 'MM') \r\n"
+	               + "              OR ?  = TO_CHAR(cal_end, 'MM'))";
+	        
+	         con = MyConnection.getConnection();
+	         pstmt = con.prepareStatement(selectSQL);
+	         pstmt.setInt(1,  employeeId);
+	         pstmt.setInt(2,  employeeId);
+			 pstmt.setString(3, ym);
+			 pstmt.setString(4, ym);
+	         rs = pstmt.executeQuery();
+	         while(rs.next()) {
+	        	
+	        	 
+	            int calRevealed = rs.getInt("cal_revealed");
+	            
+	            int calType = rs.getInt("cal_type");
+	            String calTypeName = rs.getString("cal_type_name");
+	            
+	            String calCnt = rs.getString("cal_cnt");
+	            Date calStart = rs.getDate("cal_start");
+	            Date calEnd = rs.getDate("cal_end");
+	           
+	            int departmentId = rs.getInt("department_id");
+	            
+	            String employeeNameKr = rs.getString("employee_name_kr");
+	 
+	            
+	            CalType CalType = new CalType(calType, calTypeName); 
+	            Department department = new Department(departmentId);
+	            Employee employee = new Employee(employeeId, employeeNameKr, department);
+	            Calendar cal = new Calendar(employee, calRevealed, CalType, calCnt, calStart, calEnd);
+	            calendars.add(cal);
+	            
+	            
+	            
+	         }
+	         return calendars;
+	      } catch (SQLException e) {
+	    	  e.printStackTrace();
+	         throw new SelectException(e.getMessage());
+	      }finally {
+	         MyConnection.close(rs, pstmt, con);
+	      }
+	      
+	   }
 
 }
+
--- back/src/main/java/com/my/repository/CalendarRepository.java
+++ back/src/main/java/com/my/repository/CalendarRepository.java
@@ -3,7 +3,9 @@ package com.my.repository;
 import java.util.Date;
 import java.util.List;
 
+import com.my.dto.CalType;
 import com.my.dto.Calendar;
+import com.my.dto.Employee;
 import com.my.exception.InsertException;
 import com.my.exception.SelectException;
 
@@ -20,16 +22,25 @@ public interface CalendarRepository {
 	 * 로그인한 사원의 달력정보를 반환한다
 	 * @param Month 월
 	 * @param employeeId 로그인한 사원의 사번
-	 * @param MonthButton 월 넘김 버튼 클릭 수 
 	 * @return
 	 * @throws SelectException
 	 */
-	public List<Calendar> selectCalByEmployeeId(Date Month, int employeeId, int MonthButton) throws SelectException;
+	public List<Calendar> selectCalMyExcludeDept(String ym, int employeeId) throws SelectException;
 	
 	/**
-	 * 로그인한 사원과 같은 부서 사원들의 공개체크된 달력정보를 반환한다
+	 * 로그인한 사원을 포함한 같은 부서 사원들의 공개체크된 달력정보를 반환한다
+	 * @param Month 년월 yyyy-MM
+	 * @param employeeId 로그인한 사원의 사번
 	 * @return
 	 * @throws SelectException
 	 */
-	public List<Calendar> selectCalByDepartmentId(Date Month, int employeeId,int MonthButton) throws SelectException;
+	public List<Calendar> selectCalBoth(String ym, int employeeId) throws SelectException;
+	/**
+	 * 로그인한 사원은 제외한 같은 부서 사원들의 공개체크된 달력정보를 반환한다
+	 * @param Month
+	 * @param employeeId
+	 * @return
+	 * @throws SelectException
+	 */
+	public List<Calendar> selectCalDeptExcludeMy(String ym, int employeeId) throws SelectException;
 }
