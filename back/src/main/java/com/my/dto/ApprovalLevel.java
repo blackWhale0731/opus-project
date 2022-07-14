56eab3ab41f16a73e57681bb300d9fd543e606f1
rlagusgh0223 <87258426+rlagusgh0223@users.noreply.github.com>
2022-07-14 10:19:52 +0900

    [Add] 휴가유형, 결재자 목록, 남은 휴가일수 반환, 남은 휴가일수 변경

--- back/src/main/java/com/my/dto/ApprovalLevel.java
+++ back/src/main/java/com/my/dto/ApprovalLevel.java
@@ -2,17 +2,27 @@ package com.my.dto;
 
 public class ApprovalLevel {
 	private Employee employee; //?
-	private int employee_id;
+//	private int employee_id;
+	//private String employee_name_kr;//////
 
 	public ApprovalLevel(Employee employee) {
-		employee = employee;
+		this.employee = employee;
 	} 
 	
 	///////////////////////////
-	public ApprovalLevel(int employee_id) {
-		super();
-		this.employee_id = employee_id;
-	} 
+//	public ApprovalLevel(int employee_id) {
+//		super();
+//		this.employee_id = employee_id;
+//	} 
+//	public ApprovalLevel(String employee_name_kr) {
+//		super();
+//		this.employee_name_kr = employee_name_kr;
+//	} 
+//	public ApprovalLevel(int employee_id, String employee_name_kr) {
+//		super();
+//		this.employee_id = employee_id;
+//		this.employee_name_kr = employee_name_kr;
+//	} 
 	///////////////////////////
 	
 	public Employee getEmployee() {
